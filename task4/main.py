a62e410d2cf8859845ca9136a6a7ef593a1989aa
Serhii Zhdaniuk <jdanuk@gmail.com>
2025-10-20 00:24:20 +0300

    fix: commands in task4

--- task4/main.py
+++ task4/main.py
@@ -1,51 +1,80 @@
 
-contacts = {
-    'Serhii': '0981040101'
-}
+contacts = {}
 
 def handle_help(args):
-    print("""
+    return """
 Command list:
 help   - Show this help
-new    - Add new contact
-search - Search a contact
-change - Change a contact
+add    - Add new contact. Example: add username +0981112233
+phone  - Show phone by contact. Example: phone username
+change - Change contact phone. Example: change username +0982223344
 all    - Show all contacts
 exit   - Exit the program
 close  - Close the program
-""")
+hello  - Welcome message 
+"""
 
 def add_contact(args):
-    name, phone = args
-    contacts[name] = phone
-    print("Contact added.")
+    if not len(args) == 2:
+        return "Error: Command 'add' example: add username phone"
 
-def change_contact(args):
     name, phone = args
 
     if name in contacts:
-        contacts[name] = phone
-        print('Contact changed.')
-    else:
-        contacts.setdefault(name, phone)
-        print("Contact created.")
+        return "Can't add. Username exists. Please, use command 'change' to set new phone."
+
+    contacts[name] = phone
+
+    return "Contact added."
+
+def change_contact(args):
+    if not len(args) == 2:
+        return "Error: Command 'change' example: change username phone"
+
+    name, phone = args
+
+    if name not in contacts:
+        return f"Can't change. Contact '{name}' doesn't exists."
+
+    contacts[name] = phone
+
+    return "Contact changed."
 
 def show_all(args):
-    print("Show all contacts:")
+    result = []
     for name, phone in contacts.items():
-        print(f"Name={name}, phone={phone}")
+        result.append(f"Name={name}, phone={phone}")
+    return '\n'.join(result)
 
 def handle_exit(args):
     print('Good bye!')
     exit(0)
 
+def find_by_phone(args):
+    if not len(args) == 1:
+        return "Error: Command 'phone' example: phone username"
+
+    name = args[0]
+
+
+    for contact_name, contact_phone in contacts.items():
+        if contact_name.find(name) >= 0:
+            return contact_phone
+
+    return 'Contact is not found.'
+
+def welcomme(args):
+    return 'How can I help you?'
+
 commands = {
     'help': handle_help,
-    'all': show_all,
     'add': add_contact,
+    'phone': find_by_phone,
     'change': change_contact,
+    'all': show_all,
     'exit': handle_exit,
     'close': handle_exit,
+    'hello': welcomme
 }
 
 def parse_input(user_input):
@@ -61,9 +90,9 @@ def main():
         command, *args = parse_input(user_input)
 
         if command in commands:
-            commands[command](args)
+            print(commands[command](args))
         else:
-            print(f"Invalid command. You should use one of these commands: {list(commands.keys())}")
+            print(f"Invalid command. Type 'help' to see all supported commands.")
 
 if __name__ == "__main__":
     main()
